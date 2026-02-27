0340a02e0d8455a9f43fb872e13ae7873aa015f9
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2026-02-27 14:41:38 +0000

    Improve config logging readability in main

--- cmd/appordown/main.go
+++ cmd/appordown/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/fabiant7t/appordown/internal/config"
@@ -10,9 +10,34 @@ import (
 func main() {
 	cfg, err := config.Load(os.Args[1:])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "config error: %v\n", err)
+		slog.Error("failed to load configuration", "error", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("appordown (cycle-interval=%s)\n", cfg.CycleInterval)
+	slog.Info("config",
+		"configuration_path", cfg.ConfigurationPath,
+		"cycle_interval", cfg.CycleInterval.String(),
+	)
+	slog.Info("config.http",
+		"address", cfg.HTTPServer.Address,
+		"port", cfg.HTTPServer.Port,
+		"basic_auth_username", cfg.HTTPServer.BasicAuthUsername,
+		"basic_auth_password", redact(cfg.HTTPServer.BasicAuthPassword),
+	)
+	slog.Info("config.mail",
+		"endpoint", cfg.Mailserver.Endpoint,
+		"port", cfg.Mailserver.Port,
+		"username", cfg.Mailserver.Username,
+		"password", redact(cfg.Mailserver.Password),
+		"sender", cfg.Mailserver.Sender,
+		"receivers", cfg.Mailserver.Receivers,
+		"no_tls", cfg.Mailserver.NoTLS,
+	)
+}
+
+func redact(value string) string {
+	if value == "" {
+		return ""
+	}
+	return "***"
 }
